4b964c7f4e1b9bba78632f0ad41212301cbe2798
Akash Sharma <akak9781189@gmail.com>
2025-07-14 03:27:53 +0530

    Initial commit

---
+++ recursion/day1.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+// void printName(int i,int n){
+//     if(i>n){
+//         return;
+
+//     }
+//   cout<<"raj ";
+//     printName(i+1,n);
+// }
+
+
+// void printNum(int i,int n){
+//     if(i>n){
+//         return;
+
+//     }
+//   cout<<i<<" ";
+//     printNum(i+1,n);
+// }
+
+
+
+
+// void printRev(int i,int n){
+//     if(i<1){
+//         return;
+//     }
+//     cout<<i<<" ";
+//     printRev(i-1,n);
+// }
+
+
+
+//  void printRev(int i,int n){
+//     if(i<1){
+//         return;
+//     }
+   
+//     printRev(i-1,n);
+//      cout<<i<<" ";
+// }
+
+
+
+
+// void printNum(int i,int n){
+//     if(i>n){
+//         return;
+
+//     }
+ 
+//     printNum(i+1,n);
+//      cout<<i<<" ";
+// }
+
+
+
+// void add(int n,int sum){
+//     if(n<1){
+//         cout<<sum;
+//         return;
+//     }
+
+//     add(n-1,sum+n);
+    
+// }
+
+
+int main(){
+
+    // #print name N times using recursion
+
+//     int n;
+
+//     cin>>n;
+// printName(1,n );
+
+
+
+// #print 1-n using recursion
+
+//     int n;
+
+//     cin>>n;
+// printNum(1,n );
+
+
+// print n to 1 using recursion
+
+// int n;
+// cin>>n;
+// printRev(n,n);
+
+
+// #print from 1 to n without using +
+
+
+// int n;
+// cin>>n;
+// printRev(n,n);
+
+
+// #print from 1 to n without using -
+
+//     int n;
+
+//     cin>>n;
+// printNum(1,n );
+
+
+// #paramatrized way to add 1 to n
+
+
+// int n;
+// cin>>n;
+// add(n,0);
+// }
