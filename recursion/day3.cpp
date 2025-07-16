310d7133e37808242173f47b17af8b4156f626ec
Akash Sharma <akak9781189@gmail.com>
2025-07-17 04:30:52 +0530

    Add new C++ files and update logic

---
+++ recursion/day3.cpp
@@ -0,0 +1,19 @@
+// #include <iostream>
+// using namespace std;
+
+// int MultiRecursion(int n){
+//     if(n<=1){
+//         return n;
+//     }
+//     int last=MultiRecursion(n-1);
+//     int sLast=MultiRecursion(n-2);
+//     return last+sLast;
+// }
+
+
+// int main(){
+// int P;
+// cin>>P;
+// cout<<MultiRecursion(P)<<endl;
+
+// }
---
+++ revision.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+// void Print(int i,int n){
+//     if(i>n){
+//         return;
+//     }
+//     cout<<i<<" ";
+//     Print(i+1,n);
+// }
+
+
+
+// void printRev(int i,int n){
+
+// if(i<1){
+//     return;
+// }
+// cout<<i<<" ";
+// printRev(i-1,n);
+// }
+
+
+// void Print(int i,int n){
+//     if(i<1){
+//       return;
+//     }
+//    Print(i-1,n);
+//     cout<<i<<" "; 
+
+// }
+
+
+// void print(int i ,int n){
+//     if(i>n){
+//         return;
+    
+//     }
+//     print(i+1,n);
+//     cout<<i<<" ";
+// }
+
+
+// int Sum(int n){
+//     if(n==0){
+//         return 0;
+//     }
+//     return n +Sum(n-1);
+// }
+
+// void  fact(int n,int i){
+//     if(n<1){
+//         cout<<i<<endl;
+//         return ;
+
+        
+//     }
+//     fact(n-1,i*n);
+// }
+
+// void f(int i,int arr[],int n){
+//     if(i>=n/2){
+//         return;
+//     }
+
+// swap(arr[i],arr[n-i-1]);
+// f(i+1,arr,n);
+// }
+int main(){
+
+
+//     int n;
+//     cin>>n;
+// Print(1,n);
+
+
+//   int n;
+//     cin>>n;
+// printRev(n,n);
+
+
+// int n;
+// cin>>n;
+// print(1,n);
+
+// int n;
+// cin>>n;
+// cout<<Sum(n);
+
+// int n;
+// cin>>n;
+// fact(n,1);
+
+
+//    int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     f(0,arr,n);
+    
+// for(int i=0;i<n;i++){
+//         cout<<arr[i]<<"";
+//     }
+
+}
