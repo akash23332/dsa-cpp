b8e0e95cac738bfbedc690f018c7e0620f1c76ce
Akash Sharma <akak9781189@gmail.com>
2025-07-20 10:03:53 +0530

    Re-added hashing folder with C++ files

---
+++ hashing/day1.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+
+// int hash[13]={0};
+// for(int i=0;i<n;i++){
+//     hash[arr[i]]+=1;
+// }
+
+
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         int number;
+//         cin>>number;
+//         cout<<hash[number]<<endl;
+//     }
+// }
+
+
+
+
+// FOr lower case characters
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     string s;
+//     cin>>s;
+
+// int hash[26]={0};
+// for(int i=0;i<s.size();i++){
+
+//     hash[s[i]-'a']++;
+// }
+
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         char c;
+//         cin>>c;
+//         cout<<hash[c-'a']<<endl;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+// FOr upper case characters
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     string s;
+//     cin>>s;
+
+// int hash[26]={0};
+// for(int i=0;i<s.size();i++){
+
+//     hash[s[i]-'A']++;
+// }
+
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         char c;
+//         cin>>c;
+//         cout<<hash[c-'A']<<endl;
+//     }
+
+// }
+
+
+
+
+
+
+
+// FOr all characters
+
+
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     string s;
+//     cin>>s;
+
+// int hash[256]={0};
+// for(int i=0;i<s.size();i++){
+
+//     hash[s[i]]++;
+// }
+
+
+//     int q;
+//     cin>>q;
+//     while(q--){
+//         char c;
+//         cin>>c;
+//         cout<<hash[c]<<endl;
+//     }
+
+// }
